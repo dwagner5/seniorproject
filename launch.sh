d213210b6522963ddb0594d224b9289915606dfe
dakota <electribemaster@gmail.com>
2019-04-09 13:08:37 -0500

    Create launch.sh
    this is what launches all the other scripts and is controlled by the cron job

---
+++ launch.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "Creating tunnel"
+/home/pi/scripts/remotessh/reversessh.sh
+echo "Getting MACs"
+/home/pi/scripts/MAC_Addresses/get_mac.sh
+echo "Finding remote desktop machines"
+/home/pi/scripts/remote_desktop_machines/get_remote_desktop_computer.sh
