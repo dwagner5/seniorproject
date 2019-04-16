a7aa098b00cde25d77bec710835af83e21cdec30
dakota <electribemaster@gmail.com>
2019-04-15 21:19:47 -0500

    Update NetworkDevices.sh

--- NetworkDevices.sh
+++ NetworkDevices.sh
@@ -6,7 +6,7 @@ current_time=$(date "+%Y.%m.%d-%H.%M.%S")
 
 # Get IP
 ip=$(hostname  -I | cut -f1 -d' ')
-# Set ip to a range, for example, 192.168.4.8 becomes 192.168.4.*
+# Set ip to a range, for example, 192.168.4.8 becomes 192.168.4.8-255
 ip="${ip::-1}""-255"
 
 # Scan for devices
